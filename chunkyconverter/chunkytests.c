32736745dc99c9ffe193a125bf17007bad13e881
Dr. Christian Ammann <christian.ammann@phobosys.de>
2022-04-15 16:15:28 +0200

    convert back and forth to verify functions really work

--- chunkyconverter/chunkytests.c
+++ chunkyconverter/chunkytests.c
@@ -3,9 +3,14 @@
 #define TEST_BITMAP_X 20
 #define TEST_BITMAP_Y 20
 
-void chunkyTests(void) {
-    struct BitMap *testBitmap;
+void chunkyTests(void)
+{
+    struct BitMap *sourceBitmap;
+    struct BitMap *destinationBitmap;
+
     struct p2cStruct p2c = {0};
+    struct c2pStruct c2p = {0};
+
     struct RastPort rastPort = {0};
     UBYTE *chunkyBuffer;
     UWORD i, j;
@@ -15,18 +20,40 @@ void chunkyTests(void) {
     printf("addFunc(1,2) returned: %d\n", addFunc(1, 2));
 
     // alloc memory for chunky and planar buffers
-    testBitmap = AllocBitMap(TEST_BITMAP_X, TEST_BITMAP_Y, 8,
-                             BMF_DISPLAYABLE | BMF_CLEAR, NULL);
+    sourceBitmap = AllocBitMap(TEST_BITMAP_X, TEST_BITMAP_Y, 8,
+                               BMF_DISPLAYABLE | BMF_CLEAR, NULL);
+    destinationBitmap = AllocBitMap(TEST_BITMAP_X, TEST_BITMAP_Y, 8,
+                                    BMF_DISPLAYABLE | BMF_CLEAR, NULL);
     chunkyBuffer = AllocVec(TEST_BITMAP_X * TEST_BITMAP_Y, NULL);
 
     // paint in the planar buffer
     InitRastPort(&rastPort);
-    rastPort.BitMap = testBitmap;
+    rastPort.BitMap = sourceBitmap;
     SetAPen(&rastPort, 6);
-    RectFill(&rastPort,7,7,13,13);
-    
+    RectFill(&rastPort, 7, 7, 13, 13);
+
     // convert planar buffer to chunky
-    p2c.bmap = testBitmap;
+    p2c.bmap = sourceBitmap;
+    p2c.startX = 0;
+    p2c.startY = 0;
+    p2c.width = TEST_BITMAP_X;
+    p2c.height = TEST_BITMAP_Y;
+    p2c.chunkybuffer = chunkyBuffer;
+    PlanarToChunkyAsm(&p2c);
+
+    // convert chunky back to planar
+    c2p.bmap = destinationBitmap;
+    c2p.startX = 0;
+    c2p.startY = 0;
+    c2p.width = TEST_BITMAP_X;
+    c2p.height = TEST_BITMAP_Y;
+    c2p.chunkybuffer = chunkyBuffer;
+    ChunkyToPlanarAsm(&c2p);
+
+    // clean chunky before copying back
+    memset(chunkyBuffer, 0, TEST_BITMAP_X * TEST_BITMAP_Y);
+    // convert planar buffer to chunky
+    p2c.bmap = destinationBitmap;
     p2c.startX = 0;
     p2c.startY = 0;
     p2c.width = TEST_BITMAP_X;
@@ -36,9 +63,11 @@ void chunkyTests(void) {
 
     // print content of chunky buffer on screen
     printf("chunky buffer:\n");
-    for (i = 0; i < TEST_BITMAP_X; i++) {
+    for (i = 0; i < TEST_BITMAP_X; i++)
+    {
         printf("%02d: ", i);
-        for (j = 0; j < TEST_BITMAP_Y; j++) {
+        for (j = 0; j < TEST_BITMAP_Y; j++)
+        {
             printf("%x ", chunkyBuffer[i * TEST_BITMAP_X + j]);
         }
         printf("\n");
@@ -46,5 +75,6 @@ void chunkyTests(void) {
 
     // free memory and quit
     FreeVec(chunkyBuffer);
-    FreeBitMap(testBitmap);
+    FreeBitMap(sourceBitmap);
+    FreeBitMap(destinationBitmap);
 }
